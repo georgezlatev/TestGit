06eb30d0a256e1698ed9307a133766607a7829b5
George Zlatev <george.zlatev@gmail.com>
2012-12-19 17:10:18 +0200

    maznia 2

---
+++ SpringStandalone/src/prudence/spring/beans/Person.java
@@ -0,0 +1,25 @@
+package prudence.spring.beans;
+
+public class Person {
+
+	String firstName;
+	String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
+	// this is a setter got First Name
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	//this is a second setter
+	public String getLastName() {
+		return lastName;
+	}
+
+	//this is a bla bla12121 11 third setter
+	//this is a Last 11 name Setter
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
