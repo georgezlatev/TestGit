d3d583957336255ded86ac14bb49b7b9e2b6ed6b
georgezlatev <george.zlatev@gmail.com>
2012-12-20 14:30:03 +0200

    Update SpringStandalone/src/prudence/spring/beans/Person.java

--- SpringStandalone/src/prudence/spring/beans/Person.java
+++ SpringStandalone/src/prudence/spring/beans/Person.java
@@ -7,10 +7,6 @@ public class Person {
 	public String getFirstName() {
 		return firstName;
 	}
-	// this is a setter got First Name
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
 	//this is a second setter
 	public String getLastName() {
 		return lastName;
